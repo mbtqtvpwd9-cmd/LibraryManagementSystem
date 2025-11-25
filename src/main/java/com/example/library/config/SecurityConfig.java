b039be39c3dab20d48d7efd1cb97de3ec33a9494
mbtqtvpwd9-cmd <mbtqtvpwd9@privaterelay.appleid.com>
2025-11-25 16:02:46 +0800

    Fix JwtAuthenticationFilter constructor issue
    - Remove constructor parameter from JwtAuthenticationFilter bean creation
    - Fix compilation error in SecurityConfig.java

--- src/main/java/com/example/library/config/SecurityConfig.java
+++ src/main/java/com/example/library/config/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig {
 
     @Bean
     public JwtAuthenticationFilter jwtAuthenticationFilter() {
-        return new JwtAuthenticationFilter(jwtUtil);
+        return new JwtAuthenticationFilter();
     }
 
     @Bean
